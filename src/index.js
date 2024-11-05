8c7bc474d64b74acef4ff637314d7216c8afbf0e
Arturs Anikins <xyzdelete@protonmail.com>
2024-11-05 19:46:49 +0200

    Make the app component loading

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
+import "./index.css";
+import App from "./App";
 
 import StarRating from "./StarRating";
 
@@ -19,13 +19,13 @@ function Test() {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
       defaultRating={1}
     />
     <StarRating size={24} color="red" className="test" />
-    <Test />
+    <Test /> */}
   </React.StrictMode>
 );
