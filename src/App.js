12bb7f4cc7d61493dab7184d816a13bde936f156
Arturs Anikins <xyzdelete@protonmail.com>
2024-11-07 12:24:13 +0200

    How NOT to Fetch Data in React
    Effects and Data Fetching
    
    No actual API KEY in commit for security

--- src/App.js
+++ src/App.js
@@ -47,12 +47,19 @@ const tempWatchedData = [
   },
 ];
 
+// Do not commit the actual key
+
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
 export default function App() {
-  const [movies, setMovies] = useState(tempMovieData);
-  const [watched, setWatched] = useState(tempWatchedData);
+  const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState([]);
+
+  fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+
   return (
     <>
       <NavBar>
