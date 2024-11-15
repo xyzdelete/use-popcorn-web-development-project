fd933de034ae3a375d5f9ed8da2364dd208d06dd
Arturs Anikins <xyzdelete@protonmail.com>
2024-11-15 16:11:38 +0200

    Cleaning Up Data Fetching
    Effects and Data Fetching
    
    No actual API KEY in commit for security

--- src/App.js
+++ src/App.js
@@ -96,12 +96,15 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
+
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           );
 
           if (!res.ok) {
@@ -117,7 +120,10 @@ export default function App() {
           setMovies(data.Search);
         } catch (err) {
           console.error(err.message);
-          setError(err.message);
+
+          if (err.name === "AbortError") {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -130,6 +136,11 @@ export default function App() {
       }
 
       fetchMovies();
+
+      return function () {
+        // Cleanup function
+        controller.abort();
+      };
     },
     [query]
   );
